5660921746c9956fc79421b01a029095ecad8266
ruixiang <wangruixiang07@outlook.com>
2020-06-29 20:42:00 +0200

    update quick_sort

--- quick_sort.py
+++ quick_sort.py
@@ -1,4 +1,5 @@
-nums = [-10,2,3,1,1,0,6]
+
+#frist method
 def quicksort(nums,left,right):
     if left >= right:
         return 
@@ -21,7 +22,28 @@ def quicksort(nums,left,right):
     nums[i] = standard
     quicksort(nums,left,i-1)
     quicksort(nums,i+1,right)
+#second method
+def quicksort_2(nums,left,right):
+    if left >= right:
+        return 
+    standard = nums[left]
+    i = left
+    j = right
+    while i < j:
+        while i < j and nums[j] > standard: j -= 1
+        nums[i] = nums[j]
+        while i < j and nums[i] < standard: i += 1
+        nums[j] = nums[i]
+    nums[i] = standard
+    quicksort(nums,left,i-1)
+    quicksort(nums,i+1,right)
 
+nums = [-10,2,3,1,1,0,6]
+print(nums)
 quicksort(nums, 0, len(nums)-1)
 print(nums)
 
+nums = [-10,2,3,1,1,0,6]
+print(nums)
+quicksort_2(nums, 0, len(nums)-1)
+print(nums)
